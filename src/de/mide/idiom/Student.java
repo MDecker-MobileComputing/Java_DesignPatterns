a329d08090227da6c69b0eac264097f5364cdd9a
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-02-03 13:04:17 +0100

    Example idiom example

---
+++ src/de/mide/idiom/Student.java
@@ -0,0 +1,43 @@
+package de.mide.idiom;
+
+public class Student {
+
+	private String _name;
+	
+	private int _enrollmentNumber;
+	
+	public Student( String name, int enrollmentNumber ) {
+		
+		_name             = name;
+		_enrollmentNumber = enrollmentNumber;
+	}
+	
+	public String getName() { return _name; }
+	
+	public int getEnrollmentNumber() { return _enrollmentNumber; }
+	
+	/**
+	 * The way this method is implemented can be considered as IDIOM
+	 * (but not a design pattern): It is a "programming pattern" that 
+	 * can be used in many cases and is considered as good practice,  
+	 * but is only applicable for Java).
+	 */
+	@Override
+	public boolean equals( Object object ) {
+		
+		if ( object == this ) { return true; }
+		
+		if ( object == null ) { return false; }
+		
+		if ( object instanceof Student otherStudi ) {
+		
+			return _name.equals( otherStudi.getName() ) &&
+				   _enrollmentNumber == otherStudi.getEnrollmentNumber();
+			
+		} else {
+			
+			return false;
+		}		
+	}
+	
+}
