67fe5d2113974821fcd6e6dcd271447a5827f5b4
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-03-02 17:25:46 +0100

    Update IEmployeeQuery.java

--- src/de/mide/patterns/structural/adapter/IEmployeeQuery.java
+++ src/de/mide/patterns/structural/adapter/IEmployeeQuery.java
@@ -1,10 +1,13 @@
 package de.mide.patterns.structural.adapter;
 
+import java.util.Optional;
 
 /**
  * Interface of object needed by the new client.
  */
 public interface IEmployeeQuery {
 
-	public Optional<EmployeeRecord> queryEmployy( )
+	public Optional<EmployeeRecord> queryEmployee( int employeeID ) {
+		
+	}
 }
